198bcac8fabc40f4e6a8372b040eb4656fb9c2a4
Allen <Allen>
2016-11-09 21:10:47 +0800

    set client and send fun

--- UDPDemo/ViewController.swift
+++ UDPDemo/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let client: UDPClient = UDPClient(addr: "192.168.43.3", port: 16888)
+    
+    @IBAction func b_on(_ sender: UIButton) {
+        
+        NSLog("ON")
+        sendCmd(cmd: "ON")
+    }
+    
+    @IBAction func b_off(_ sender: UIButton) {
+        
+        NSLog("OFF")
+        sendCmd(cmd: "OFF")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        let (success, msg) = client.close()
+        
+        if (success) {
+            print(msg)
+        }
+        else {
+            print("Error. " + msg)
+        }
+    }
+    
+    func sendCmd(cmd: String) {
+        
+        let (success, msg) = client.send(str: cmd)
+        
+        if (success) {
+            print(msg)
+        }
+        else {
+            print("Error. " + msg)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
