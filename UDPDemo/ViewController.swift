fff4a1541486994ab21f91bcf6de9e3e70a1ee9d
Allen <Allen>
2016-11-09 20:47:44 +0800

    Initial Commit

---
+++ UDPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UDPDemo
+//
+//  Created by 許家旗 on 2016/11/9.
+//  Copyright © 2016年 許家旗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
